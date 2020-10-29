513c3fad0ecfca1ee37034461ab16325648cfd7c
eternityxxxx <gpavlikov7@gmail.com>
2020-10-29 11:45:27 +0600

    Add view CarList and change urls.py

--- car/urls.py
+++ car/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import CarListView
 
 
 urlpatterns = [
-    path(''),
+    path('list/', CarListView.as_view(), name='car_list'),
 ]
--- car/views.py
+++ car/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Car
 
-# Create your views here.
+
+class CarListView(ListView):
+    model = Car
+    context_object_name = 'car_list'
+    template_name = 'index.html'
