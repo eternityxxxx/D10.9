7a265adaca1ae58428fc3dea19e961b2f4850a88
eternityxxxx <gpavlikov7@gmail.com>
2020-10-29 08:54:25 +0600

    Start new app Car, add models, configure project settings

---
+++ car/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Car(models.Model):
+    TRANSMISSIONS = (
+        (1, 'Механика'),
+        (2, 'Автомат'),
+        (3, 'Робот'),
+    )
+
+    make = models.CharField(max_length=128, verbose_name='Марка')
+    model = models.CharField(max_length=32, verbose_name='Модель')
+    year = models.IntegerField(verbose_name='Год выпуска')
+    transmission = models.SmallIntegerField(choices=TRANSMISSIONS, verbose_name='Коробка передач')
+    color = models.CharField(max_length=32, verbose_name='Цвет')
