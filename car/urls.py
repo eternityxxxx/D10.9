94f226a588212df8190d692ad164e3b46b0a5a7f
eternityxxxx <gpavlikov7@gmail.com>
2020-10-30 17:45:59 +0600

    Add search

--- car/urls.py
+++ car/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CarListView
+from .views import CarListView, CarLSearchView
 
 
 urlpatterns = [
     path('list/', CarListView.as_view(), name='car_list'),
+    path('search/', CarLSearchView.as_view(), name='car_search')
 ]
--- car/views.py
+++ car/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.db.models import Q
 from .models import Car
 
 
@@ -6,3 +7,24 @@ class CarListView(ListView):
     model = Car
     context_object_name = 'car_list'
     template_name = 'index.html'
+
+
+class CarLSearchView(ListView):
+    model = Car
+    context_object_name = 'car_list'
+    template_name = 'index.html'
+
+    def get_queryset(self):
+        car_make = self.request.GET.get('make')
+        car_model = self.request.GET.get('model')
+        car_year = self.request.GET.get('year')
+        car_transmission = self.request.GET.get('transmission')
+        car_color = self.request.GET.get('color')
+
+        return Car.objects.filter(
+            Q(make__icontains=car_make) &
+            Q(model__icontains=car_model) &
+            Q(year__icontains=car_year) &
+            Q(transmission__icontains=car_transmission) &
+            Q(color__icontains=car_color)
+        )
